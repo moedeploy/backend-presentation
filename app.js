ffd6e7005b62f8344aac21564ee1512677e6fdf6
moedeploy <smileymoehd@gmail.com>
2023-03-08 02:15:30 +0000

    changed port

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ async function promptUser() {
 
     const options = {
       hostname: '172.30.198.77',
-      port: 8080,
+      port: process.env.port || 8080,
       path: '/',
       method: 'POST',
       headers: {
